5cf0dc94df9fa3a54dae33967d6f5345c7edf0b7
Dione Wilson <dionew1@gmail.com>
2017-12-22 12:55:29 -0700

    Add pagination to category show page

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,6 @@
 class CategoriesController < ApplicationController
   def show
-    @category = Category.friendly.find(params[:category])
+    category = Category.friendly.find(params[:category])
+    @category_items = category.items.paginate(page: params[:page], per_page: 20)
  end
 end
